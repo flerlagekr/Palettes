f4b2b1535b49574d8f0bb0b4c21ce9a5ebad24b8
Ken Flerlage <flerlagekr@gmail.com>
2021-05-09 09:52:41 -0400

    20210509 Update comments.

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 #  Written by Ken Flerlage, April, 2021
 #
 #  Generate a Tableau color palette file based on crowdsourced palettes from the Tableau "Datafam"
+#  File is written to a public S3 bucket and can be downloaded here: https://flerlage-apps.s3.us-east-2.amazonaws.com/Preferences.tps
 #
 #  This code is in the public domain
 
